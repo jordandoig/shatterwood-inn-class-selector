5bd2392207f87f53e014d521d017c6b527d47a7c
Jordan Doig <jordandoig@live.com>
2019-04-08 22:59:18 -0600

    Add header/footer skeletons

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Footer.css'
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="appFooter">
+        I'm putting my foot down!!!!
+      </footer>
+    );
+  }
+}
+
+export default Footer;
