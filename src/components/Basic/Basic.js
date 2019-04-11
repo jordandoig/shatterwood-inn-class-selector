78e16c08c25144310aaaa577f524114794d9080b
Jordan Doig <jordandoig@live.com>
2019-04-10 20:51:44 -0600

    Add flow functionality, add basic data json

--- src/components/Basic/Basic.js
+++ src/components/Basic/Basic.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
-import './Basic.css'
+import { Link } from 'react-router-dom';
+import basicData from '../../assets/class-flows/basicData.json';
+import './Basic.css';
 
-class Basic extends Component {
+export default class Basic extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: basicData
+    }
+  }
+
   render() {
     return (
       <main className="basicContainer">
-        Here is the basicContainer!!!
+        <header>{ this.state.data.question }</header>
+        { this.renderOptions() }
       </main>
     );
   }
-}
 
-export default Basic;
+  renderOptions() {
+    let options = this.state.data.options;
+
+    let optionsJSX = options.map((item, index, options) => {
+      if ( item.class ) {
+        return (
+          <Link to={ '/' + item.class } key={ 'options_' + index }>
+            <p>{item.answer}</p>
+          </Link>
+        );
+      } else {
+        return (
+          <div key={'options_' + index} onClick={this.handleSelection.bind( this, index )}>
+            <p>{ item.answer }</p>
+          </div>
+        );
+      }
+    });
+
+    return optionsJSX;
+  }
+
+  handleSelection( index ) {
+    let newData = this.state.data.options[ index ];
+    if ( newData.class ) {
+      this.setState({ data: basicData });
+
+    } else {
+      this.setState({ data: newData });
+    }
+  }
+}
