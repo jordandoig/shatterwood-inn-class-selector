2f9a71d4c2759e8f9b7866a96e24f5c4b9df12e6
Jordan Doig <jordandoig@live.com>
2019-04-25 00:45:41 -0600

    Stylin

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,11 @@ class Footer extends Component {
   render() {
     return (
       <footer className="appFooter">
-        I'm putting my foot down!!!!
+        <div className="linkContainer">
+          <a href="https://www.venmo.com/Jordan-Doig-1">Buy me a coffee</a>
+          <a href="https://www.jordandoig.com">About the developer</a>
+        </div>
+        <i>&#169;2019 Jordan Doig</i>
       </footer>
     );
   }
